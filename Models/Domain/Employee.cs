7914485ca2043745cdd44486efb044cbf6363e1a
Ahmed-Imad <ahmedfattal82@gmail.com>
2022-11-09 02:49:51 +0400

    Add project files.

---
+++ Models/Domain/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPNETMVCCRUD.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public long Salary { get; set; }
+        public string Department { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
